6ec5424aa0733163c283b34133cfa7d2b814e682
sravan <bskreddy1990@gmail.com>
2019-05-21 11:58:45 +0530

    first commit

--- src/main/java/com/github/britter/springbootherokudemo/config/WebMvcConfig.java
+++ src/main/java/com/github/britter/springbootherokudemo/config/WebMvcConfig.java
@@ -15,47 +15,32 @@
  */
 package com.github.britter.springbootherokudemo.config;
 
- 
-import org.springframework.context.MessageSource;
+ import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
- 
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
- 
-      
-    @Bean(name = "localeResolver")
-    public LocaleResolver getLocaleResolver()  {
-        CookieLocaleResolver resolver= new CookieLocaleResolver();
-        resolver.setCookieDomain("myAppLocaleCookie");
-        // 60 minutes 
-        resolver.setCookieMaxAge(60*60); 
-        return resolver;
-    } 
-     
-    @Bean(name = "messageSource")
-    public MessageSource getMessageResource()  {
-        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
-         
-        // Read i18n/messages_xxx.properties file.
-        // For example: i18n/messages_en.properties
-        messageResource.setBasename("classpath:i18n/messages");
-        messageResource.setDefaultEncoding("UTF-8");
-        return messageResource;
-    }
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
-        localeInterceptor.setParamName("lang");
-         
-         
-        registry.addInterceptor(localeInterceptor).addPathPatterns("/*");
-    }
-     
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+   @Bean
+   public LocaleResolver localeResolver() {
+      SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+      sessionLocaleResolver.setDefaultLocale(Locale.US);
+      return sessionLocaleResolver;
+   }
+   @Bean
+   public LocaleChangeInterceptor localeChangeInterceptor() {
+      LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+      localeChangeInterceptor.setParamName("language");
+      return localeChangeInterceptor;
+   }
+   @Override
+   public void addInterceptors(InterceptorRegistry registry) {
+      registry.addInterceptor(localeChangeInterceptor());
+   }
 }
