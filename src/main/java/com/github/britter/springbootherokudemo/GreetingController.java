fc75f90f6410571ff4ad439070c31398dd681349
sravan <bskreddy1990@gmail.com>
2019-05-21 11:27:37 +0530

    first commit

--- src/main/java/com/github/britter/springbootherokudemo/GreetingController.java
+++ src/main/java/com/github/britter/springbootherokudemo/GreetingController.java
@@ -17,13 +17,13 @@ package com.github.britter.springbootherokudemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "home";
