3783ca18c81b1846a8efabbf74611ea42a2e0cdc
sravan <bskreddy1990@gmail.com>
2019-05-22 00:23:37 +0530

    first commit

--- src/main/java/com/github/britter/springbootherokudemo/Config.java
+++ src/main/java/com/github/britter/springbootherokudemo/Config.java
@@ -15,10 +15,13 @@
  */
 package com.github.britter.springbootherokudemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -27,5 +30,12 @@ public class Config extends WebMvcConfigurerAdapter {
     public void configureDefaultServletHandling(
             DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/html");
+        resolver.setSuffix(".html");
+        return resolver;
     }
 }
