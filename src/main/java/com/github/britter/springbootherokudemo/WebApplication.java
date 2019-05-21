0ba4ee272d1f04ef40594d8307c7f604470cfc08
sravan <bskreddy1990@gmail.com>
2019-05-22 02:00:21 +0530

    first commit

--- src/main/java/com/github/britter/springbootherokudemo/WebApplication.java
+++ src/main/java/com/github/britter/springbootherokudemo/WebApplication.java
@@ -18,7 +18,7 @@ package com.github.britter.springbootherokudemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.github.britter.springbootherokudemo")
 public class WebApplication extends SpringBootServletInitializer {
