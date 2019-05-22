d2993bc149c41fb35154cb9f80145c5f7a18b8ff
sravan <bskreddy1990@gmail.com>
2019-05-22 11:53:37 +0530

    first commit

--- src/main/java/com/github/britter/springbootherokudemo/WebSecurityConfig.java
+++ src/main/java/com/github/britter/springbootherokudemo/WebSecurityConfig.java
@@ -23,7 +23,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
- 
+import org.springframework.http.HttpMethod;
+
 @Configuration
 @EnableAutoConfiguration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -43,9 +44,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     http.authorizeRequests().antMatchers("/", "/home").permitAll().antMatchers("/admin").hasRole("ADMIN")
         .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
         .permitAll();
-http
-                //HTTP Basic authentication
-                .httpBasic()
+	http.httpBasic()
                 .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/rest/home/").hasRole("USER")
