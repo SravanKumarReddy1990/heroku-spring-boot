82d824b5c0455f88e1215a0984588055a3117329
sravan <bskreddy1990@gmail.com>
2019-05-27 13:09:02 +0530

    first commit

--- src/main/java/com/github/britter/springbootherokudemo/controller/WebController.java
+++ src/main/java/com/github/britter/springbootherokudemo/controller/WebController.java
@@ -21,7 +21,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.github.britter.springbootherokudemo.utill.EmailUtil;
+import com.github.britter.springbootherokudemo.util.EmailUtil;
 
 @Controller
 public class WebController {
