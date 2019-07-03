968fc2db0b710429dea5324ee06730dfab1d4640
sravan <bskreddy1990@gmail.com>
2019-07-03 21:09:06 +0530

    first commit

---
+++ src/main/resources/static/js/profilejs.js
@@ -0,0 +1,99 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="section">
+    <div class="container">
+    	<div class="row">
+            <div class="col-md-12">
+    	    </div>
+    	</div>
+    	<div class="row">
+    		<div class="col-md-4 first">
+    		    <div class="promote">
+            	    <h1 class="text-center h2"><span>Bootstrap 4 <br/>Social Profile Cards </span><br/>Created with ♥ <br/> from <a href="https://shurvir.blogspot.com/?utm_source=bootsnipp&utm_medium=bootsnipp" target="_blank">shurvir.blogspot .com</a><br/> by Shurvir Mori</h1>
+                </div>
+		    </div>
+    		<div class="col-md-4">
+    		    <div class="card profile-card-2">
+                    <div class="card-img-block">
+                        <img class="img-fluid" src="https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&h=350" alt="Card image cap" />
+                    </div>
+                    <div class="card-body pt-5">
+                        <img src="https://randomuser.me/api/portraits/women/81.jpg" alt="profile-image" class="profile"/>
+                        <h5 class="card-title">Shurvir Mori</h5>
+                        <p class="card-text">Lorem Ipsum is simply dummy text Lorem Ipsum has been the industry's standard dummy text</p>
+                        <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-google-plus"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+    		<div class="col-md-4">
+    		    <div class="card profile-card-2">
+                    <div class="card-img-block">
+                        <img class="img-fluid" src="https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&h=350" alt="Card image cap" />
+                    </div>
+                    <div class="card-body pt-5">
+                        <img src="https://randomuser.me/api/portraits/women/81.jpg" alt="profile-image" class="profile"/>
+                        <h5 class="card-title">Shurvir Mori</h5>
+                        <p class="card-text">Lorem Ipsum is simply dummy text Lorem Ipsum has been the industry's standard dummy text</p>
+                        <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-google-plus"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+    		<div class="col-md-4">
+    		    <div class="card profile-card-2">
+                    <div class="card-img-block">
+                        <img class="img-fluid" src="https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&h=350" alt="Card image cap" />
+                    </div>
+                    <div class="card-body pt-5">
+                        <img src="https://randomuser.me/api/portraits/women/81.jpg" alt="profile-image" class="profile"/>
+                        <h5 class="card-title">Shurvir Mori</h5>
+                        <p class="card-text">Lorem Ipsum is simply dummy text Lorem Ipsum has been the industry's standard dummy text</p>
+                        <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-google-plus"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+    		<div class="col-md-4">
+    		    <div class="card profile-card-2">
+                    <div class="card-img-block">
+                        <img class="img-fluid" src="https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&h=350" alt="Card image cap" />
+                    </div>
+                    <div class="card-body pt-5">
+                        <img src="https://randomuser.me/api/portraits/women/81.jpg" alt="profile-image" class="profile"/>
+                        <h5 class="card-title">Shurvir Mori</h5>
+                        <p class="card-text">Lorem Ipsum is simply dummy text Lorem Ipsum has been the industry's standard dummy text</p>
+                        <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-google-plus"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+    		<div class="col-md-4">
+    		    <div class="card profile-card-2">
+                    <div class="card-img-block">
+                        <img class="img-fluid" src="https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&h=350" alt="Card image cap" />
+                    </div>
+                    <div class="card-body pt-5">
+                        <img src="https://randomuser.me/api/portraits/women/81.jpg" alt="profile-image" class="profile"/>
+                        <h5 class="card-title">Shurvir Mori</h5>
+                        <p class="card-text">Lorem Ipsum is simply dummy text Lorem Ipsum has been the industry's standard dummy text</p>
+                        <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-google-plus"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+    		<div class="col-md-4">
+    		    <div class="card profile-card-2">
+                    <div class="card-img-block">
+                        <img class="img-fluid" src="https://images.pexels.com/photos/877695/pexels-photo-877695.jpeg?auto=compress&cs=tinysrgb&h=350" alt="Card image cap" />
+                    </div>
+                    <div class="card-body pt-5">
+                        <img src="https://randomuser.me/api/portraits/women/81.jpg" alt="profile-image" class="profile"/>
+                        <h5 class="card-title">Shurvir Mori</h5>
+                        <p class="card-text">Lorem Ipsum is simply dummy text Lorem Ipsum has been the industry's standard dummy text</p>
+                        <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-google-plus"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+    	    
+    	</div>
+    </div>
+</div>
